a7f5cee94b9ed743b3584cc65c0709e5e9a4d516
fredkendy <fredkendy9@gmail.com>
2023-10-02 15:42:28 -0300

    componentes

--- src/index.js
+++ src/index.js
@@ -1,10 +1,13 @@
-// Importar módulos
+// Importar módulos 
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-// Sem JSX
-// const elemento = React.createElement('h1', {}, 'Sem JSX');
-// ReactDOM.render(elemento, document.getElementById('root'));
+// Componente (transformar classe em componente)
+class MeuComponente extends React.Component{
+    render() {
+        return <h1>Meu primeiro componente</h1>;
+    }
+}
 
-// Com JSX
-ReactDOM.render(<h1>Com JSX</h1>, document.getElementById('root'));
+//Render (chamando componente no root)
+ReactDOM.render(<MeuComponente />, document.getElementById('root'));
