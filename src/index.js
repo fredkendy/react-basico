e2b8a99f716dcaf54bd7c9cd444d5d889a3f1856
fredkendy <fredkendy9@gmail.com>
2023-10-03 11:03:43 -0300

    listas e chaves

--- src/index.js
+++ src/index.js
@@ -5,38 +5,24 @@ import ReactDOM from 'react-dom';
 // Componente
 class MeuComponente extends React.Component {
 
-    // Construtor
-    constructor(props) {
-        super(props);
-
-        this.state = {exibir: false};
-    }
-
-    // Função
-    alterarState = () => {
-        let converter = !this.state.exibir;
-        this.setState({exibir: converter});
-    }
-
-    // Render
     render() {
 
-        // Condicional
-        let meuTexto = '';
-        if (this.state.exibir == true) {
-            meuTexto = <h1>Olá</h1>
-        } else {
-            meuTexto = <h1>Tchau</h1>
-        }
+        // Vetor
+        let cores = ['azul', 'amarelo', 'vermelho'];
+
+        // Função: percorrer cada elemento
+        let listar = cores.map((cor, index) => {
+            return <li key={index}> {index} - {cor} </li> //chave garante que o elemento percorrido é único
+        });
 
         return(
-            <div>
-                {meuTexto}
-                <button onClick={this.alterarState}>Clique Aqui</button>
-            </div>
+            <ul>
+                {listar}
+            </ul>
         )
-    }
 
+    }
+    
 }
 
 ReactDOM.render(<MeuComponente />, document.getElementById('root'));
